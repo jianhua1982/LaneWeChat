4d5d0f1f765a95a339809c1dafa8b798e9f898bc
jianhua1982 <andyccnu0829>
2015-11-26 10:04:11 +0800

    signature implementation.

--- core/signature.lib.php
+++ core/signature.lib.php
@@ -6,9 +6,6 @@
  * Time: PM7:20
  */
 
-require('HttpUtil.php');
-require_once 'Simple-PHP-Cache/cache.class.php';
-
 /**
  * 微信公众平台 JS_SDK 签名
  *
@@ -22,25 +19,20 @@ class Signature
      */
     private $appId;
     private $appSecret;
-    private $mem;
     private $url;
     private $cache;
 
-    public function __construct($appId, $appSecret, $mem, $url) {
+    public function __construct($appId, $appSecret, $url) {
         $this->appId = $appId;
         $this->appSecret = $appSecret;
-        $this->mem = $mem;
         $this->url = $url;
-
-        $this->cache = new Cache();
     }
-
 
     /**
      * 生成签名
      */
     public function getSignPackage() {
-        $jsapiTicket = $this->getJsApiTicket();
+        $jsapiTicket = \LaneWeChat\Core\ApiTicket::getApiTicket();
         $timestamp = time();
         $nonceStr = $this->createNonceStr();
 
@@ -57,59 +49,6 @@ class Signature
             "signature" => $signature
         );
         return $signPackage;
-    }
-
-    /**
-     * 获取api_ticket  jsapi_ticket 应该全局存储与更新，读写memcache
-     */
-    private function getJsApiTicket() {
-
-        $ticketKey = 'wxwallet_jsapi_ticket';
-
-        $data = $this->cache->retrieve($ticketKey);
-        if (!$data) {
-            $accessToken = $this->getAccessToken();
-            $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
-            $res = json_decode(HttpUtil::sendGetRequet($url));
-            if($res){
-                $ticket = $res->ticket;
-                if($ticket){
-                    //$this->mem->set($ticketKey, $ticket, 0, 7000);
-                    $this->cache->store($ticketKey, $ticket, 7000);
-                }
-            }
-        }else {
-            $ticket = $data;
-        }
-        return $ticket;
-    }
-
-    /**
-     * 获取access_token  access_token应该全局存储与更新，读写memcache
-     */
-    private function getAccessToken() {
-
-        $tokenKey = 'wxwallet_access_token';
-
-        //$data = $this->mem->get($tokenKey);
-        $data = $this->cache->retrieve($tokenKey);
-
-        //var_dump($data);
-
-        if (!$data) {
-            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
-            $res = json_decode(HttpUtil::sendGetRequet($url));
-            if($res){
-                $access_token = $res->access_token;
-                if($access_token) {
-                    //$this->mem->set($tokenKey, $access_token, 0, 7000);
-                    $this->cache->store($tokenKey, $access_token, 7000 );
-                }
-            }
-        }else{
-            $access_token = $data;
-        }
-        return $access_token;
     }
 
     /**

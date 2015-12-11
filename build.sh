53111d0f55af3151f67f7bf5efa9858636048ad8
jianhua1982 <andyccnu0829>
2015-12-11 20:33:39 +0800

    keep stability.

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 
 # set vars
 productionDir="Production"
+myDate=`date +%Y%m%d%H%M%S`
 
-rm -rf ${productionDir} && gulp release && cp *.php ${productionDir} && mkdir ${productionDir}/core && cp -rf core/* ${productionDir}/core
+rm -rf ${productionDir} && gulp release && cp *.php ${productionDir} && mkdir ${productionDir}/core && cp -rf core/* ${productionDir}/core && mv ${productionDir} ${productionDir}.${myDate} && echo ${productionDir}.${myDate}成功构建
